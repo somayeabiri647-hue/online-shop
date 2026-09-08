8f51c629820242ef809b9632fe2f2dc276c3ba69
Ali Asghar <somayeabiri647@gmail.com>
2026-09-08 20:27:47 +0330

    add sqlalchemy database

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from blueprint.general import app as general
+from blueprint.admin import app as admin
+from blueprint.user import app as user
+import config
+
 
 app = Flask(__name__)
-
+app.register_blueprint(user)
+app.register_blueprint(admin)
 app.register_blueprint(general)
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 
-@app.route("/")
-def home():
-    return "hello world"
+db = SQLAlchemy()
+db.init_app(app)
 
-app.run(host="0.0.0.0", port=5000, debug=True)
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run()

4c2e7c62d5da24a9e719b82b8a78fd6dc446b757
Ali Asghar <somayeabiri647@gmail.com>
2026-09-12 20:17:48 +0330

    update project

--- blueprint/admin.py
+++ blueprint/admin.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 app = Blueprint("admin", __name__)
 
-@app.route("/admin")
-def main():
-    return "this is admin page"
+@app.route("/admin/login")
+def login():
+    return render_template('admin/login.html')

23e85039c70ecdacb4726c7f209cc8a7bdf57b1b
Ali Asghar <somayeabiri647@gmail.com>
2026-09-13 19:27:10 +0330

    update project

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
-
 from blueprint.general import app as general
 from blueprint.admin import app as admin
 from blueprint.user import app as user
@@ -9,15 +9,16 @@ import config
 import extention
 
 app = Flask(__name__)
-
 app.register_blueprint(user)
 app.register_blueprint(admin)
 app.register_blueprint(general)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 app.config["SECRET_KEY"] = config.SECRET_KEY
-
 extention.db.init_app(app)
+
+csrf = CSRFProtect(app)
+
 
 with app.app_context():
     extention.db.create_all()

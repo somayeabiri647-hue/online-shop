64343e94d3b6be9abe2a9fde6ff012505a788775
Ali Asghar <somayeabiri647@gmail.com>
2026-09-14 19:56:32 +0330

    login and product

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
-
+    return User.query.get(int(user_id))
 
 
 with app.app_context():
--- blueprint/user.py
+++ blueprint/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template , request , redirect
+from flask import Blueprint, render_template , request , redirect , url_for , flash
 from flask_login import login_user
 from passlib.hash import sha256_crypt
 from models.user import User
@@ -13,10 +13,10 @@ def login():
         return render_template("user/login.html")
     else:
         register = request.form.get("register" , None)
-        username = request.form.get("username")
-        password = request.form.get("password")
-        phone = request.form.get("phone")
-        address = request.form.get("address")
+        username = request.form.get("username", None)
+        password = request.form.get("password", None)
+        phone = request.form.get("phone", None)
+        address = request.form.get("address", None)
 
         if request != None:
             user = User(username = username , password= sha256_crypt.encrypt(password) , phone = phone , address = address)
@@ -26,4 +26,12 @@ def login():
 
             return redirect("/user/dashboard")
 
+        else:
+            user = User.query.filter(User.username == username).first()
+            if user == None:
+                flash("نام کاربری یا رمز اشتباه است")
+                return redirect(url_for("user.login"))
+
+            if sha256_crypt.ve 
+    
         return "ok"
